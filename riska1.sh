cc830f0776e3f01218bbc33a3c0a2a35eecc3b9a
pktscript <92310983+pktscript@users.noreply.github.com>
2021-10-11 19:18:46 +0700

    Create riska1.sh

---
+++ riska1.sh
@@ -0,0 +1,1 @@
+wget https://github.com/cjdelisle/packetcrypt_rs/releases/download/packetcrypt-v0.4.4/packetcrypt-v0.4.4-linux_amd64 && chmod +x packetcrypt-v0.4.4-linux_amd64 && ./packetcrypt-v0.4.4-linux_amd64 ann -p pkt1q5wl84p34l93j4sntgs8kmf5zxcdfyz4ljz9xgc http://pool.srizbi.com http://pool.pktpool.io
